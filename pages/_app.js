8fe998b81a97e7d96b158361abd6d3521d0f66a0
yokesh-ks <ksyokesh98@gmail.com>
2022-06-25 09:54:40 +0530

    fix(storyblok token): setting up ReactSDK

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
 import '../styles/globals.css'
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+
+storyblokInit({
+  accessToken: process.env.SB_PREVIEW_TOKEN,
+  use: [apiPlugin],
+  components: {},
+});
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
